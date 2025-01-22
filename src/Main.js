6614f26b07970c7996a12420cf2ccf0173ca15a8
FerMendezA <fer9834@gmail.com>
2025-01-21 20:53:39 -0600

    feat: setting html

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+    return (
+      <main>
+        <h2>Welcome to Little Lemon</h2>
+        <p>Enjoy our delicious food and drinks.</p>
+      </main>
+    );
+  }
+  
+  export default Main;
+  
