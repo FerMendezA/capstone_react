390e1aab94b3952b8752ec0f529394eb564884df
FerMendezA <fer9834@gmail.com>
2025-01-22 01:54:40 -0600

    feat: booking and tests

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Header from './Header';
 import HomePage from './Homepage';
 import Footer from './Footer';
 import {Route,Routes} from "react-router-dom"
-import BookingPage from './BookingPage';
 
 function App() {
   return (
     <div className='container'>
       <Routes> 
       <Route path="/" exact element={<HomePage />}></Route>
-      <Route path="/booking" element={<BookingPage />}></Route> 
+      {/*<Route path="/booking" element={<BookingPage />}></Route>*/}
       </Routes>
     </div>
   );
